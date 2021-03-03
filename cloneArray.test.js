8edde2bb0f20819e02f9b60a239c590ef9b13a61
softwaree40 <softwaree40@gmail.com>
2021-03-03 00:54:53 -0500

    testing array that reference different memory space

---
+++ cloneArray.test.js
@@ -0,0 +1,7 @@
+const { expect } = require("@jest/globals")
+const cloneArray = require("./cloneArray")
+
+test("create new array from the old one",()=>{
+    const array = [1,2,3,4,5,6]
+    expect(cloneArray(array)).toStrictEqual(array)
+})
